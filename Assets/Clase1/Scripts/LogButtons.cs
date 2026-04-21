a1deead9e72b4bf0923bde4f4780f1b39f8122b9
rbenitez2 <benitezhrodrigo@gmail.com>
2026-04-20 23:58:30 -0300

    Tarea 1,2 y 3 Finalizada, Se crearon los distintos mensajes con los colores para cada evento de click en los botones y para el msj de Error se agrego un break

--- Assets/Clase1/Scripts/LogButtons.cs
+++ Assets/Clase1/Scripts/LogButtons.cs
@@ -6,19 +6,20 @@ public class LogButtons : MonoBehaviour
     // TAREA 1: Haz que este botón muestre un mensaje normal en consola (color blanco)
     public void ShowLogInfoMessage()
     {
-        // Escribe tu código aquí:
+        Debug.Log("Este es un mensaje normal y deberia salir en BLANCO");
     }
 
     // TAREA 2: Haz que este botón muestre una ADVERTENCIA (triángulo amarillo)
     public void ShowLogWarningMessage()
     {
-        // Escribe tu código aquí:
+        Debug.LogWarning("Este es un mensaje normal y deberia salir en AMARILLO");
     }
 
     // TAREA 3: Haz que este botón muestre un ERROR (octágono rojo)
     // PLUS: Investiga cómo hacer que Unity se PAUSE automáticamente al pulsar este botón.
     public void ShowLogErrorMessage()
     {
-        // Escribe tu código aquí:
+        Debug.LogError("Este es un mensaje normal y deberia salir en ROJO");
+        Debug.Break();
     }
 }
